29ca2ca4e18755bf25512aed7911d6e1617d244e
AndreGomesSilva <silva_andregomes@outlook.com>
2022-10-03 20:12:05 -0300

    Updates

---
+++ precache-manifest.e7a02df378f3239c3196b0b397c49a38.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "f626a9fca33ec9d7d625",
+    "url": "/alura-geek/static/css/main.14d16204.chunk.css"
+  },
+  {
+    "revision": "f626a9fca33ec9d7d625",
+    "url": "/alura-geek/static/js/main.f626a9fc.chunk.js"
+  },
+  {
+    "revision": "bbc98ac6a496cebb6bf4",
+    "url": "/alura-geek/static/js/1.bbc98ac6.chunk.js"
+  },
+  {
+    "revision": "00228b47f32504932576",
+    "url": "/alura-geek/static/js/runtime~main.00228b47.js"
+  },
+  {
+    "revision": "74db7980c5cbfaab7d186279b8781820",
+    "url": "/alura-geek/static/media/banner-image.74db7980.png"
+  },
+  {
+    "revision": "452263e2587f5aba949fb55613e130fe",
+    "url": "/alura-geek/static/media/produto-1.452263e2.jpg"
+  },
+  {
+    "revision": "eab5232b5cd62b8c0941cf72aee5da97",
+    "url": "/alura-geek/static/media/produto-2.eab5232b.jpg"
+  },
+  {
+    "revision": "095617e802d96b5f1890be1a70831634",
+    "url": "/alura-geek/static/media/produto-3.095617e8.jpg"
+  },
+  {
+    "revision": "f678aacc1e0db77841e41fe7aefd988b",
+    "url": "/alura-geek/static/media/produto-4.f678aacc.jpg"
+  },
+  {
+    "revision": "202394778d2177dd971b40f937cefcbb",
+    "url": "/alura-geek/static/media/produto-5.20239477.jpg"
+  },
+  {
+    "revision": "ff81a94cb051b1945a590e42c7cb41f8",
+    "url": "/alura-geek/static/media/produto-6.ff81a94c.jpg"
+  },
+  {
+    "revision": "60360000b73417a813dfc31bd4a0ac98",
+    "url": "/alura-geek/static/media/produto-2.60360000.jpg"
+  },
+  {
+    "revision": "2c9eb431c841c3c2120de6377a4b5600",
+    "url": "/alura-geek/static/media/produto-3.2c9eb431.jpg"
+  },
+  {
+    "revision": "19e8b871abc8d4ba46897de532477a56",
+    "url": "/alura-geek/static/media/produto-4.19e8b871.jpg"
+  },
+  {
+    "revision": "003454b61ff5fcb03a8d3470d3c236ab",
+    "url": "/alura-geek/static/media/produto-6.003454b6.jpg"
+  },
+  {
+    "revision": "43b581c0524c74961018d3d3198134e1",
+    "url": "/alura-geek/static/media/produto-1.43b581c0.jpg"
+  },
+  {
+    "revision": "9fcd201ad4be154b6ba53201d4d0ec86",
+    "url": "/alura-geek/static/media/produto-2.9fcd201a.jpg"
+  },
+  {
+    "revision": "cf4623a6848039baf05b72540b730fbe",
+    "url": "/alura-geek/static/media/produto-3.cf4623a6.jpg"
+  },
+  {
+    "revision": "36bc38e429c01453ce1aa6c6a019c26f",
+    "url": "/alura-geek/static/media/produto-4.36bc38e4.jpg"
+  },
+  {
+    "revision": "c3ca8052b5d65207b32809b636a69fb2",
+    "url": "/alura-geek/static/media/produto-5.c3ca8052.jpg"
+  },
+  {
+    "revision": "02df34346fa3431369371fb2327d8133",
+    "url": "/alura-geek/static/media/produto-6.02df3434.jpg"
+  },
+  {
+    "revision": "1a354f60b1b0be1ae6f1f42f16ee7449",
+    "url": "/alura-geek/index.html"
+  }
+];
